0eb18060abdf7a1c43ab29dd2c10527217d82c2d
RomaGor <goryachevskii60@gmail.com>
2022-06-19 15:39:33 +0300

    HW-2.1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 10;
+        int minRubles = 20;
+
+        if (ticketPrice >= 20) {
+            System.out.println((ticketPrice / minRubles) + " miles");
+        } else {
+            System.out.println((ticketPrice / minRubles) + " miles");
+        }
+    }
 }
